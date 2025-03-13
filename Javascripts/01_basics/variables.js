a180913dc07d36f5896a659a9e56ed37d00bf7ef
techads <jambhaleaditya91@gmail.com>
2025-03-13 13:04:40 +0530

    basics and variables

---
+++ Javascripts/01_basics/variables.js
@@ -0,0 +1,14 @@
+//explain the var , let and const 
+
+var name = "Aditya"
+// console.log(name);
+
+if(true){
+    var name = "this is inside the block"
+
+}
+console.log(name);//leaks out 
+
+
+
+//explaining the let 
